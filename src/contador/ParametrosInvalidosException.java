ec359e5cf206beea1729ad7ffb195072f7cfc4f4
big-rodrigo <151597068+big-rodrigo@users.noreply.github.com>
2024-07-25 14:10:20 -0300

    Add files via upload

---
+++ src/contador/ParametrosInvalidosException.java
@@ -0,0 +1,10 @@
+package contador;
+
+public class ParametrosInvalidosException extends Exception {
+
+	public ParametrosInvalidosException(String msg) {
+		super(msg);
+	}
+
+	private static final long serialVersionUID = 1L;
+}
